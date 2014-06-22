d32e1863f480f0ba18aee18503f029f6624e14e9
efimich <efimich007@yandex.ru>
2014-06-22 14:37:28 +0400

    some minor fix

--- superego/_config_exmaple.php
+++ superego/_config_exmaple.php
@@ -2,7 +2,7 @@
 $hostname = "localhost";
 $db_username = "XXXXXXX";
 $db_password = "XXXXXXX";
-$db_database = "XXXXX"
+$db_database = "XXXXX";
 
 $link = mysql_connect($hostname, $db_username, $db_password) or die("Cannot connect to the database");
 mysql_select_db($db_database) or die("Cannot select the database");
